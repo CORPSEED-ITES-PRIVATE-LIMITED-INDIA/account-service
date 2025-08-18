d64dabc486da5ee7eee6f81ee1ba1b3e0407b68c
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-08-18 08:34:13 +0530

    add changes

--- src/main/java/com/account/dashboard/domain/BankStatement.java
+++ src/main/java/com/account/dashboard/domain/BankStatement.java
@@ -46,6 +46,78 @@ public class BankStatement {
 			inverseJoinColumns = {@JoinColumn(name="bank_statement_payment_register_id"
 					+ "",referencedColumnName = "id",nullable=true,unique=false)})
 	List<PaymentRegister>paymentRegister;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public double getLeftAmount() {
+		return leftAmount;
+	}
+
+	public void setLeftAmount(double leftAmount) {
+		this.leftAmount = leftAmount;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getPaymentDate() {
+		return paymentDate;
+	}
+
+	public void setPaymentDate(Date paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public List<PaymentRegister> getPaymentRegister() {
+		return paymentRegister;
+	}
+
+	public void setPaymentRegister(List<PaymentRegister> paymentRegister) {
+		this.paymentRegister = paymentRegister;
+	}
 	
 	
 	
