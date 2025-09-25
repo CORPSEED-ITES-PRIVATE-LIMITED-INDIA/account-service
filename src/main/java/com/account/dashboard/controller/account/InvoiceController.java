3abfe64a7fda1ed1acc99ae152d9ff321094f962
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-09-25 12:39:14 +0530

    add changes

--- src/main/java/com/account/dashboard/controller/account/InvoiceController.java
+++ src/main/java/com/account/dashboard/controller/account/InvoiceController.java
@@ -6,10 +6,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.account.dashboard.service.InvoiceService;
 import com.account.dashboard.util.UrlsMapping;
-
+@RestController
 public class InvoiceController {
 
 	
