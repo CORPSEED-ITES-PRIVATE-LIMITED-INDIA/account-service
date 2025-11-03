eb315f7690f43c96e8d94574bfd0ed4eee968f6c
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-11-03 08:27:03 +0530

    add changes

--- src/main/java/com/account/dashboard/controller/paymentRegister/GstDataCrmController.java
+++ src/main/java/com/account/dashboard/controller/paymentRegister/GstDataCrmController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.account.dashboard.dto.AddGstDto;
 import com.account.dashboard.service.GstDataCrmService;
 import com.account.dashboard.util.UrlsMapping;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 public class GstDataCrmController {
@@ -21,6 +24,12 @@ public class GstDataCrmController {
 	public List<Map<String,Object>> getAllGstDataCrm(@RequestParam(value = "page", defaultValue = "1") int page,
 			@RequestParam(value = "size", defaultValue = "10") int size){
 		List<Map<String,Object>> res=gstDataCrmService.getAllGstDataCrm(page-1,size);	
+		return res;
+		
+	}
+	@GetMapping(UrlsMapping.ADD_GST_DATA_CRM)
+	public Boolean addGstDataCrm(@RequestBody AddGstDto addGstDto){
+		Boolean res=gstDataCrmService.addGstDataCrm(addGstDto);	
 		return res;
 		
 	}
--- src/main/java/com/account/dashboard/serviceImpl/GstDataCrmServiceImpl.java
+++ src/main/java/com/account/dashboard/serviceImpl/GstDataCrmServiceImpl.java
@@ -1,10 +1,62 @@
 package com.account.dashboard.serviceImpl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.account.dashboard.domain.GstDataFromCrm;
+import com.account.dashboard.domain.PaymentRegister;
+import com.account.dashboard.dto.AddGstDto;
+import com.account.dashboard.repository.GstDataFromCrmRepository;
+import com.account.dashboard.repository.PaymentRegisterRepository;
 import com.account.dashboard.service.GstDataCrmService;
 
 @Service
 public class GstDataCrmServiceImpl implements GstDataCrmService{
+	
+	@Autowired
+	GstDataFromCrmRepository gstDataFromCrmRepository;
+	@Autowired
+	PaymentRegisterRepository paymentRegisterRepository;
+
+	@Override
+	public List<Map<String, Object>> getAllGstDataCrm(int i, int size) {
+		List<GstDataFromCrm> gstData = gstDataFromCrmRepository.findAll();
+		List<Map<String, Object>> list = gstData.stream().map(m -> {
+		    Map<String, Object> map = new HashMap<>();
+		    map.put("id", m.getId());
+		    map.put("paymentRegister", m.getPaymentRegister());
+		    map.put("gst", m.getGst());
+		    map.put("gstAmount", m.getGstAmount());
+		    map.put("company", m.getCompany());
+		    map.put("type", m.getType());
+		    map.put("status", m.getStatus());
+		    map.put("document", m.getDocument());
+		    return map;
+		}).collect(Collectors.toList());
+
+		return list;
+	}
+
+	@Override
+	public Boolean addGstDataCrm(AddGstDto addGstDto) {
+		Boolean flag=false;
+		GstDataFromCrm gstDataFromCrm=new GstDataFromCrm();
+		gstDataFromCrm.setCompany(addGstDto.getCompany());
+		gstDataFromCrm.setGst(addGstDto.getGst());
+		gstDataFromCrm.setGstAmount(addGstDto.getGstAmount());
+		PaymentRegister paymentRegister = paymentRegisterRepository.findById(addGstDto.getPaymentRegisterId()).get();
+//		gstDataFromCrm.setPaymentRegister(addGstDto.getPaymentRegisterId());
+		gstDataFromCrm.setStatus(addGstDto.getStatus());
+		gstDataFromCrm.setType(addGstDto.getType());
+		gstDataFromCrmRepository.save(gstDataFromCrm);
+		flag=true;
+		return flag;
+	}
 
 }
