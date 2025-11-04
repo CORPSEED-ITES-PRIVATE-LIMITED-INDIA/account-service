8f3885513f279a72addd81e65b95c4a09fc309e8
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-11-04 12:21:44 +0530

    add changes in erp

--- src/main/java/com/account/dashboard/repository/GstDataFromCrmRepository.java
+++ src/main/java/com/account/dashboard/repository/GstDataFromCrmRepository.java
@@ -14,13 +14,13 @@ import com.account.dashboard.domain.GstDetails;
 @Repository
 public interface GstDataFromCrmRepository extends JpaRepository<GstDataFromCrm, Long> {
 
-	@Query(value = "SELECT * FROM gst_data_form_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
+	@Query(value = "SELECT * FROM gst_data_from_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
 	Page<GstDataFromCrm> findAllByDateInBetween(String d1, String d2, Pageable pageable);
 	
-	@Query(value = "SELECT * FROM gst_data_form_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
+	@Query(value = "SELECT * FROM gst_data_from_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
 	List<GstDataFromCrm> findAllByDateInBetween(String d1, String d2);
 	
-	@Query(value = "SELECT count(*) FROM gst_data_form_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
+	@Query(value = "SELECT count(*) FROM gst_data_from_crm gd WHERE gd.create_date BETWEEN :d1 AND :d2", nativeQuery = true)
 	Long findCountByDateInBetween(String d1, String d2);
 
 }
