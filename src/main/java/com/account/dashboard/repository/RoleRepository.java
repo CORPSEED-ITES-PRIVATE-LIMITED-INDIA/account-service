6a6c57c78578467236501571a3c554b5b10ee413
AryanChaurasiaCorpseed <aryan.chaurasia@corpseed.com>
2025-12-11 15:51:26 +0530

    add chnages in erp

--- src/main/java/com/account/dashboard/repository/RoleRepository.java
+++ src/main/java/com/account/dashboard/repository/RoleRepository.java
@@ -12,5 +12,8 @@ import com.account.dashboard.domain.Role;
 public interface RoleRepository extends JpaRepository<Role, Long> {
 	@Query(value = "SELECT * FROM roles r WHERE r.name in(:name)", nativeQuery = true)
 	List<Role> findAllByNameIn(List<String> name);
+	
+	@Query(value = "SELECT * FROM roles r WHERE r.name =:name limit 1", nativeQuery = true)
+	Role findAllByName(String name);
 
 }
--- src/main/java/com/account/dashboard/serviceImpl/ImportEstimateServiceImpl.java
+++ src/main/java/com/account/dashboard/serviceImpl/ImportEstimateServiceImpl.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
@@ -17,11 +18,13 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.account.dashboard.dto.GraphDateFilter;
+import com.account.dashboard.repository.RoleRepository;
 import com.account.dashboard.repository.UserRepository;
 import com.account.dashboard.service.ImportEstimateService;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.S3Object;
 import com.account.dashboard.domain.PaymentRegister;
+import com.account.dashboard.domain.Role;
 import com.account.dashboard.domain.User;
 import com.account.dashboard.domain.User.*;
 
@@ -29,8 +32,11 @@ import com.account.dashboard.domain.User.*;
 @Service
 public class ImportEstimateServiceImpl implements ImportEstimateService{
 
-    private final UserRepository userRepository;
-	
+	@Autowired
+    UserRepository userRepository;
+    
+	@Autowired
+    RoleRepository roleRepository;
 
 
 	@Value("${aws.s3.bucket-name}")
@@ -48,9 +54,9 @@ public class ImportEstimateServiceImpl implements ImportEstimateService{
 	@Autowired
 	private AmazonS3 amazonS3Client;
 
-    ImportEstimateServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+//    ImportEstimateServiceImpl(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
 
 	@Override
 	public List<Map<String, Object>> importEstimateData(String s3Url) {
@@ -91,12 +97,24 @@ public class ImportEstimateServiceImpl implements ImportEstimateService{
 			String userDepartment = record.get("userDepartment").toString();
 			String userDesignation = record.get("userDesignation").toString();
 			String userRole = record.get("userRole").toString();
-			
-			System.out.println("User Name   "+userName);
-			System.out.println("User Email   "+userEmail);
-			System.out.println("User Department   "+userDepartment);
-			System.out.println("User Designation   "+userDesignation);
-	
+			if(userName!=null && (!userName.equals(""))) {
+				User u=new User();
+				u.setId(1l);
+				u.setFullName(userName);
+				u.setEmail(userEmail);
+				u.setDepartment(userDepartment);
+				u.setDesignation(userDesignation);
+				Role role = roleRepository.findAllByName(userRole);
+				List<Role>rList=new ArrayList<>();
+				rList.add(role);
+				u.setRole(Arrays.asList(userRole));
+//				System.out.println("user role . "+userRole);
+//				System.out.println("role . "+role);
+
+				u.setUserRole(rList);
+				userRepository.save(u);
+			}
+
 		}
 		return userList;
 	}
