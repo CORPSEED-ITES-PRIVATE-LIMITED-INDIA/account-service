7bc48a0a86a836be60a71dbfd2e874b161b3f428
Kaushlendra Pratap <kaushlendra.pratap@corpseed.com>
2026-04-07 10:14:36 +0530

    fix: block payment registration when estimate is REJECTED

--- src/main/java/com/account/AccountServiceApplication.java
+++ src/main/java/com/account/AccountServiceApplication.java
@@ -33,7 +33,6 @@ public class AccountServiceApplication {
 	 * only if they do not already exist (checked by 'code').
 	 */
 
-
 	@Bean
 	public CommandLineRunner seedPaymentTypes() {
 		return args -> {
